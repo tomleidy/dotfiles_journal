51e54cb3a0f7ce08584d2d4c4dca801b2a838ed3
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-12-03 18:11:56 -0800

    chore: tidying up journal.sh, comments

--- scripts/journal.sh
+++ scripts/journal.sh
@@ -21,42 +21,41 @@ esac
 # Construct the filename
 title="${year}$(date +%m)$(date +%d) ${weekday} the ${day}${ordinal} of ${month}"
 filename=${title}.txt
-
 cur_os=$(which_os)
-# macOS
+
+
+# Determine paths based on OS
 if [ "$cur_os" = "macOS" ]; then
     JOURNAL_DIR=~/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents/Morning\ Pages
-    editor_command='open -a "iA Writer"'
 elif [ "$cur_os" = "Windows" ]; then
     JOURNAL_DIR=~/iCloudDrive/27N4MQEA55~pro~writer/Morning\ Pages
-    editor_command='/c/Program Files/iA Writer/iAWriter.exe'
 fi
-path_and_file="${JOURNAL_DIR}/${filename}"
-questions="${JOURNAL_DIR}/questions.txt"
+morning_page_file="${JOURNAL_DIR}/${filename}"
+questions_file="${JOURNAL_DIR}/questions.txt"
 
-# create file
-if [ ! -e "$path_and_file" ]; then
-    echo $title >> "$path_and_file"
-    echo "#MorningPages" >> "$path_and_file"
-    echo >> "$path_and_file"
+# Create today's entry if it doesn't exist
+if [ ! -e "$morning_page_file" ]; then
+    echo $title >> "$morning_page_file"
+    echo "#MorningPages" >> "$morning_page_file"
+    echo >> "$morning_page_file"
 
-    # add questions if they exist
-    if [ -e "$questions" ]; then
-        echo "$questions"
-        word_count1=$(wc -w < "$path_and_file")
-        word_count2=$(cat "$questions" | sed s/^-// | wc -w)
+    # Add questions if they exist
+    if [ -e "$questions_file" ]; then
+        echo "$questions_file"
+        word_count1=$(wc -w < "$morning_page_file")
+        word_count2=$(cat "$questions_file" | sed s/^-// | wc -w)
         total_word_count=$((word_count1 + word_count2 + 753))
-        echo >> "$path_and_file"
-        echo >> "$path_and_file"
-        echo "Goal WC: $total_word_count" >> "$path_and_file"
-        cat "$questions" >> "$path_and_file"
+        echo >> "$morning_page_file"
+        echo >> "$morning_page_file"
+        echo "Goal WC: $total_word_count" >> "$morning_page_file"
+        cat "$questions_file" >> "$morning_page_file"
         sleep 1
     fi
 fi
-echo "$path_and_file"
 
+# Execute iA Writer based on OS
 if [ "$cur_os" = "macOS" ]; then
-    open -a "iA Writer" "$path_and_file"
+    open -a "iA Writer" "$morning_page_file"
 elif [ "$cur_os" = "Windows" ]; then
-    '/c/Program Files/iA Writer/iAWriter.exe' "$path_and_file" &
+    '/c/Program Files/iA Writer/iAWriter.exe' "$morning_page_file" &
 fi
