da5245e22615628c5f93a2fc925029806721654b
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-12-31 09:37:21 -0800

    refactor: brought which_os and wordcount functions back

--- scripts/journal.sh
+++ scripts/journal.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-source ~/.dot/helper/which_os
-source ~/.dot/helper/get_wordcount.sh
+
+which_os() {
+    local OS=$(uname -s)
+    case "$OS" in
+        CYGWIN*|MINGW*|MSYS*)
+            echo "Windows"
+            ;;
+        Darwin)
+            echo "macOS"
+            ;;
+        Linux)
+            echo "Linux"
+            ;;
+        *)
+            echo "Unknown"
+            ;;
+    esac
+}
+
+get_approximate_ia_writer_wordcount_from_file() {
+    local non_word_strip1=$(sed -e 's/[_:\>\<\/]/ /g' \
+                                -e 's/[A-Za-z]\/[A-Za-z]/ /g' \
+                                -e 's/[&—-]//g' \
+                                -e 's/\([0-9]\)\’\([[:alpha:]]\)/\1 \2/g' \
+                                -e 's/ […\?]/…/g' \
+                                -e 's/\([[:alnum:]]\)\.\([[:alnum:]]\)/\1 \2/g' "$1")
+    local wordcount=$(echo $non_word_strip1 | wc -w | tr -d '[:blank:]')
+    echo $wordcount
+}
+
+
+EVENING_HOUR_STARTS=18
 
 # Get today's date components
 get_title_today() {
